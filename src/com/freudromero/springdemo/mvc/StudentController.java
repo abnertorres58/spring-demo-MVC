169d48ef502178236abf520eb2194393699b7dd7
fred <abnertorres58@gmail.com>
2022-04-05 19:49:45 -0400

    Use Properties file to load Country Options

--- src/com/freudromero/springdemo/mvc/StudentController.java
+++ src/com/freudromero/springdemo/mvc/StudentController.java
@@ -1,21 +1,31 @@
 package com.freudromero.springdemo.mvc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
+    @Value("#{countryOptions}")
+    private Map<String, String> countryOptions;
+
     @RequestMapping("/showForm")
     public String showForm(Model theModel) {
-        // Create a student Object
+
+        // create a student object Student
         Student theStudent = new Student();
 
-        // Add student object to the model
+        // add student object to the model
         theModel.addAttribute("student", theStudent);
+
+        // add the country options to the model
+        theModel.addAttribute("theCountryOptions", countryOptions);
 
         return "student-form";
     }
--- web/WEB-INF/view/student-form.jsp
+++ web/WEB-INF/view/student-form.jsp
@@ -20,7 +20,7 @@
         <br><br>
         Country:
         <form:select path="country">
-            <form:options items="${student.countryOptions}"/>
+            <form:options items="${theCountryOptions}"/>
         </form:select>
         <br><br>
         <input type="submit" value="Submit"/>
