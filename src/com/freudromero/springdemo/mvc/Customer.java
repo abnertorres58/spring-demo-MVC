2b59169721e5652cefd8e75733fc1d87dc3a7f96
fred <abnertorres58@gmail.com>
2022-04-06 17:55:36 -0400

    How to make an Integer field required and handling Strings

--- src/com/freudromero/springdemo/mvc/Customer.java
+++ src/com/freudromero/springdemo/mvc/Customer.java
@@ -10,9 +10,10 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName;
 
+    @NotNull(message="is required")
     @Min(value = 0, message = "must be greater or equal to zero")
     @Max(value = 10, message = "must be less then or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
@@ -33,11 +34,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
